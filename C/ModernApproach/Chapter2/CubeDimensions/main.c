2f2660f3a90dd036723d0c61061740ccacd72329
Chris Ubben <ubben.chris@gmail.com>
2020-09-11 12:36:54 -0700

    Added user input to CubeDimensions.

--- C/ModernApproach/Chapter2/CubeDimensions/main.c
+++ C/ModernApproach/Chapter2/CubeDimensions/main.c
@@ -4,13 +4,21 @@
 // Date created: 09/11/2020                                                    
 //=============================================================================
 
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main(void)
 {
-    int length = 8; // length of the cube in inches
-    int width = 12; // width of the cube in inches
-    int height = 10; // height of the cube in inches
+    int length; // length of the cube in inches
+    int width; // width of the cube in inches
+    int height; // height of the cube in inches
+
+    printf("Enter length of cube: ");
+    scanf("%d", &length);
+    printf("Enter width of cube: ");
+    scanf("%d", &width);
+    printf("Enter height of cube: ");
+    scanf("%d", &height);
 
     int volume = length * width * height; // volume of the cube in cubic inches
 
