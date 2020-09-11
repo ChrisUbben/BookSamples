b13624dc5f8f8b80d5d5bb25eb8b6b8f1d114f9e
Chris Ubben <ubben.chris@gmail.com>
2020-09-11 13:11:16 -0700

    Added CheckMark project.

---
+++ C/ModernApproach/Chapter2/CheckMark/main.c
@@ -0,0 +1,21 @@
+//=============================================================================
+// Filename : main.c                                                           
+// Author: Chris Ubben                                                         
+// Date created: 09/11/2020                                                    
+//=============================================================================
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("       *\n");
+    printf("      * \n");
+    printf("     *  \n");
+    printf("*   *   \n");
+    printf(" * *    \n");
+    printf("  *     \n");
+
+    return 0;
+}
