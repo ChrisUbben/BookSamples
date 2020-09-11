2a268a72353384ad4dd0eae397143c19361f131b
Chris Ubben <ubben.chris@gmail.com>
2020-09-11 13:33:57 -0700

    Added BillSplitter project.

---
+++ C/ModernApproach/Chapter2/BillSplitter/main.c
@@ -0,0 +1,36 @@
+//=============================================================================
+// Filename : main.c                                                           
+// Author: Chris Ubben                                                         
+// Date created: 09/11/2020                                                    
+//=============================================================================
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#define SALES_TAX_RATE 0.079f
+
+#include <stdio.h>
+
+int main(void)
+{
+    int subtotal;
+    printf("Enter dollar amount to split: ");
+    scanf("%d", &subtotal);
+
+    int twenties = subtotal / 20;
+    subtotal -= twenties * 20;
+
+    int tens = subtotal / 10;
+    subtotal -= tens * 10;
+
+    int fives = subtotal / 5;
+    subtotal -= fives * 5;
+
+    int ones = subtotal;
+
+    printf("$20 bills: %d\n", twenties);
+    printf("$10 bills: %d\n", tens);
+    printf("$5 bills: %d\n", fives);
+    printf("$1 bills: %d\n", ones);
+
+    return 0;
+}
